6af6d285c7fbe1b13ff293579629beb10b427734
Milad <miladabdi1374@gmail.com>
2023-04-14 12:04:03 +0330

    Add project files.

---
+++ Data Structures And Algorithms/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// O(1) : Constant
+Console.WriteLine("Hello, World!");
+
+// O(n) : Linear
+var numbers = new int[] { 1, 3, 5, 7 };
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
+
+// O(n ^ 2) : Quadratic
+for (int i = 0; i < numbers.Length; i++)
+{
+    for (int j = 0; j < numbers.Length; j++)
+    {
+        Console.WriteLine(numbers[i] + ", " + numbers[j]);
+    }
+}
+
+// O(n ^ 3) : Quadratic
+for (int i = 0; i < numbers.Length; i++)
+{
+    for (int j = 0; j < numbers.Length; j++)
+    {
+        for (int k = 0; k < numbers.Length; k++)
+        {
+            
+        }
+    }
+}
+
+// O(log n) like ---> binary search : Logarithmic
+
+// O(2 ^ n) : Exponential
