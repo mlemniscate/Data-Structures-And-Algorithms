7ce99ff91fa1bc4ebad949cd49525227e7066169
Milad <miladabdi1374@gmail.com>
2023-04-15 21:53:34 +0330

    feature: Add 5 method to MyLinkedList (AddLast, DeleteFirst, DeleteLast, Contains, IndexOf)

--- Data Structures And Algorithms/Linked Lists/MyLinkedList.cs
+++ Data Structures And Algorithms/Linked Lists/MyLinkedList.cs
@@ -24,7 +24,7 @@ public class MyLinkedList
         if (first == null)
         {
             first = new Node(value);
-            last = first;
+            last ??= first;
         }
         else
         {
@@ -34,6 +34,63 @@ public class MyLinkedList
                 Next = oldFirst
             };
         }
+    }
+
+    public void AddLast(int value)
+    {
+        if (last == null)
+        {
+            last = new Node(value);
+            first ??= last;
+        }
+        else
+        {
+            var oldLast = last;
+            last = new Node(value);
+            oldLast.Next = last;
+        }
+    }
+
+    public void DeleteFirst()
+    {
+        first = first.Next;
+    }
+
+    public void DeleteLast()
+    {
+        var node = first;
+        while (node.Next != last)
+        {
+            node = node.Next;
+        }
+        last = node;
+        last.Next = null;
+    }
+
+    public bool Contains(int value)
+    {
+        var node = first;
+         while (node != null)
+        {
+            if (node.Value.Equals(value))
+                return true;
+            node = node.Next;
+        }
+        return false;
+    }
+
+    public int IndexOf(int value)
+    {
+        var node = first;
+        var index = 0;
+        while (node != null)
+        {
+            if (node.Value.Equals(value))
+                return index;
+            node = node.Next;
+            index++;
+        }
+        return -1;
     }
 
     public void Print()
@@ -67,9 +124,16 @@ public class MyLinkedList
         // Console.WriteLine("Contain: " + list.Contains(5));
 
         var myLinkedList = new MyLinkedList();
+        myLinkedList.AddLast(-10);
+        myLinkedList.AddLast(-20);
         myLinkedList.AddFirst(10);
         myLinkedList.AddFirst(20);
         myLinkedList.AddFirst(30);
+        myLinkedList.DeleteLast();
+        Console.WriteLine(myLinkedList.Contains(30));
+        Console.WriteLine(myLinkedList.Contains(-20));
+        Console.WriteLine(myLinkedList.IndexOf(-10));
+        Console.WriteLine(myLinkedList.IndexOf(-20));
         myLinkedList.Print();
     }
 }
